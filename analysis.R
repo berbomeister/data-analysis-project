9812933323a5b402005125beb448ae1156d9f121
Kliment <kberbatov9@gmail.com>
2025-10-06 12:50:39 +0200

    update analysis.r

--- analysis.R
+++ analysis.R
@@ -1,4 +1,4 @@
-# Initial data exploration     
+# Initial data exploration for iris dataset 
 data(iris)     
 summary(iris)
 plot(iris$Sepal.Length, iris$Sepal.Width, main="Iris Sepal Dimensions")
