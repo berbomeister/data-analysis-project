b5e849d450aedbfd86cbbcfaca1303f0f643e23b
Kliment Berbatov <kberbatov9@gmail.com>
2025-10-06 12:52:21 +0200

    conflict test

--- analysis.R
+++ analysis.R
@@ -1,4 +1,4 @@
-# Initial data exploration for iris dataset 
+# R-Script for the initial exploration of the Iris dataset
 data(iris)     
 summary(iris)
 plot(iris$Sepal.Length, iris$Sepal.Width, main="Iris Sepal Dimensions")
