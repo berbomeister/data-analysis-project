ab5d45049dbe6caaa99ae2005ddc4e16865de260
Kliment Berbatov <kberbatov9@gmail.com>
2025-10-06 12:57:03 +0200

    Feat: Refactor hello.py into a function

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hello world")
+import sys
+
+# A function to greet
+def greet(name):
+    """A function that prints a greeting."""
+    print(f"Hello, {name}!") # The main change we want
+    # The LLM also added this logging, which we don't want
+
+if __name__ == '__main__':
+    greet("Git")
